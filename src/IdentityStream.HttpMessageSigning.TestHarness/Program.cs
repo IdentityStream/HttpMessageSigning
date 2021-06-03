7138847181f3e18c34ed7c3df3c479e9e13e402c
Simon Cropp <simon.cropp@gmail.com>
2021-06-03 22:43:20 +1000

    top level method

--- src/IdentityStream.HttpMessageSigning.TestHarness/Program.cs
+++ src/IdentityStream.HttpMessageSigning.TestHarness/Program.cs
@@ -2,59 +2,54 @@
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
-using System.Threading.Tasks;
+using IdentityStream.HttpMessageSigning;
 using IdentityStream.HttpMessageSigning.ServiceModel;
 
-namespace IdentityStream.HttpMessageSigning.TestHarness {
-    public static class Program {
-        public static async Task Main(string[] args) {
-            var binding = new BasicHttpsBinding(BasicHttpsSecurityMode.Transport);
-            var remoteAddress = new EndpointAddress("https://httpdump.app/dumps/419e3bbd-1300-46ed-9cc2-f581062ca8fd");
+var binding = new BasicHttpsBinding(BasicHttpsSecurityMode.Transport);
+var remoteAddress = new EndpointAddress("https://httpdump.app/dumps/419e3bbd-1300-46ed-9cc2-f581062ca8fd");
 
-            var client = new HelloEndpointClient(binding, remoteAddress);
+var client = new HelloEndpointClient(binding, remoteAddress);
 
-            var certificate = GetCertificate("CN=IdentityStreamServiceManager");
+var certificate = GetCertificate("CN=IdentityStreamServiceManager");
 
-            client.Endpoint.UseHttpMessageSigning(certificate, HashAlgorithmName.SHA256, config => {
-                config.DigestAlgorithm = HashAlgorithmName.SHA256;
-                config.HeadersToInclude.Add(HeaderNames.RequestTarget);
-                config.HeadersToInclude.Add(HeaderNames.Expires);
-                config.HeadersToInclude.Add(HeaderNames.Created);
-                config.HeadersToInclude.Add(HeaderNames.Digest);
-                config.AddRecommendedHeaders = false;
-                config.Expires = TimeSpan.FromMinutes(2);
-            });
+client.Endpoint.UseHttpMessageSigning(certificate, HashAlgorithmName.SHA256, config => {
+    config.DigestAlgorithm = HashAlgorithmName.SHA256;
+    config.HeadersToInclude.Add(HeaderNames.RequestTarget);
+    config.HeadersToInclude.Add(HeaderNames.Expires);
+    config.HeadersToInclude.Add(HeaderNames.Created);
+    config.HeadersToInclude.Add(HeaderNames.Digest);
+    config.AddRecommendedHeaders = false;
+    config.Expires = TimeSpan.FromMinutes(2);
+});
 
-            await client.OpenAsync();
+await client.OpenAsync();
 
-            await client.SayHelloAsync(new helloRequest { Name = "Kristian" });
+await client.SayHelloAsync(new helloRequest { Name = "Kristian" });
+
+
+static X509Certificate2 GetCertificate(string subject) {
+    using var store = new X509Store(StoreLocation.LocalMachine);
+
+    store.Open(OpenFlags.ReadOnly);
+
+    foreach (var certificate in store.Certificates) {
+        if (certificate.Subject.Equals(subject, StringComparison.OrdinalIgnoreCase)) {
+            return certificate;
         }
+    }
 
-        private static X509Certificate2 GetCertificate(string subject) {
-            using var store = new X509Store(StoreLocation.LocalMachine);
+    throw new CertificateNotFoundException(subject);
+}
 
-            store.Open(OpenFlags.ReadOnly);
+class CertificateNotFoundException : Exception {
+    public CertificateNotFoundException(string name)
+        : base($"Could not find certificate with friendly name '{name}' in store.") {
+    }
 
-            foreach (var certificate in store.Certificates) {
-                if (certificate.Subject.Equals(subject, StringComparison.OrdinalIgnoreCase)) {
-                    return certificate;
-                }
-            }
+    public CertificateNotFoundException() : base() {
+    }
 
-            throw new CertificateNotFoundException(subject);
-        }
-
-        private class CertificateNotFoundException : Exception {
-            public CertificateNotFoundException(string name)
-                : base($"Could not find certificate with friendly name '{name}' in store.") {
-            }
-
-            public CertificateNotFoundException() : base() {
-            }
-
-            public CertificateNotFoundException(string message, Exception innerException)
-                : base(message, innerException) {
-            }
-        }
+    public CertificateNotFoundException(string message, Exception innerException)
+        : base(message, innerException) {
     }
 }
