7dd27de2eb07ad3cf14ac4578ebaa6b81916b758
Kristian Hellang <kristian@identitystream.com>
2021-06-04 10:59:38 +0200

    Add DelegatingHandler for use with HttpClient

---
+++ src/IdentityStream.HttpMessageSigning/SigningHttpMessageHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityStream.HttpMessageSigning {
+    /// <summary>
+    /// A delegating handler to be used with <see cref="HttpClient"/> for signing HTTP requests.
+    /// </summary>
+    public class SigningHttpMessageHandler : DelegatingHandler {
+        /// <summary>
+        /// Creates an instance of <see cref="SigningHttpMessageHandler"/> with the specified <paramref name="config"/>.
+        /// </summary>
+        /// <param name="config">The configuration to use when signing HTTP requests.</param>
+        public SigningHttpMessageHandler(HttpMessageSigningConfiguration config) {
+            Config = config;
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="SigningHttpMessageHandler"/> with the specified <paramref name="config"/>
+        /// and <paramref name="innerHandler"/>.
+        /// </summary>
+        /// <param name="config">The configuration to use when signing HTTP requests.</param>
+        /// <param name="innerHandler">The inner handler which is responsible for processing the HTTP response messages.</param>
+        public SigningHttpMessageHandler(HttpMessageSigningConfiguration config, HttpMessageHandler innerHandler)
+            : base(innerHandler) {
+            Config = config;
+        }
+
+        private HttpMessageSigningConfiguration Config { get; }
+
+        /// <inheritdoc/>
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+            var message = new HttpMessage(request);
+
+            await HttpMessageSigner.SignAsync(message, Config).ConfigureAwait(false);
+
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+
+        private class HttpMessage : IHttpMessage {
+            public HttpMessage(HttpRequestMessage request) {
+                Request = request;
+            }
+
+            public HttpContent? Content => Request.Content;
+
+            public HttpMethod Method => Request.Method;
+
+            public Uri RequestUri => Request.RequestUri;
+
+            private HttpRequestMessage Request { get; }
+
+            public void SetHeader(string name, string value) =>
+                Request.Headers.TryAddWithoutValidation(name, value);
+
+            public bool TryGetHeaderValues(string name, [NotNullWhen(true)] out IEnumerable<string>? values) =>
+                Request.Headers.TryGetValues(name, out values);
+        }
+    }
+}
---
+++ test/IdentityStream.HttpMessageSigning.Tests/HttpMessageSigningHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IdentityStream.HttpMessageSigning.Tests {
+    public class HttpMessageSigningHandlerTests {
+        [Fact]
+        public async Task IncludedHeaders_AreAddedToRequest() {
+            var signatureAlgorithm = new TestSignatureAlgorithm(HashAlgorithmName.SHA512);
+
+            var config = HttpMessageSigningConfiguration.Create("d4db0d", signatureAlgorithm, config => {
+                config.GetCurrentTimestamp = () => new DateTimeOffset(2021, 05, 27, 10, 23, 00, TimeSpan.Zero);
+                config.DigestAlgorithm = HashAlgorithmName.SHA256;
+                config.HeadersToInclude.Add(HeaderNames.Date);
+                config.HeadersToInclude.Add(HeaderNames.Host);
+            });
+
+            var recorder = new RecordingHttpRequestHandler();
+
+            var handler = new SigningHttpMessageHandler(config, recorder);
+
+            using var client = new HttpClient(handler);
+
+            await client.GetAsync("https://identitystream.com/hello");
+
+            var request = recorder.Requests.Single();
+
+            Assert.True(request.Headers.Contains(HeaderNames.Signature));
+            Assert.True(request.Headers.Contains(HeaderNames.Digest));
+            Assert.True(request.Headers.Contains(HeaderNames.Host));
+            Assert.True(request.Headers.Contains(HeaderNames.Date));
+        }
+
+        /// <summary>
+        /// A request handler that records all requests and sends back an empty 200 OK response.
+        /// </summary>
+        private class RecordingHttpRequestHandler : DelegatingHandler {
+            public RecordingHttpRequestHandler() {
+                Requests = new Queue<HttpRequestMessage>();
+            }
+
+            public Queue<HttpRequestMessage> Requests { get; }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+                Requests.Enqueue(request);
+                return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+            }
+        }
+    }
+}
