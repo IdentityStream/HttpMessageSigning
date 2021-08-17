d38c1c64de20c3dad13e703a9bb7424a34cf5b81
Kristian Hellang <kristian@identitystream.com>
2021-08-17 17:30:46 +0200

    Move HttpRequestMessage signing into extension method

--- src/IdentityStream.HttpMessageSigning/SigningHttpMessageHandler.cs
+++ src/IdentityStream.HttpMessageSigning/SigningHttpMessageHandler.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -34,38 +32,8 @@ namespace IdentityStream.HttpMessageSigning {
 
         /// <inheritdoc/>
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
-            var message = new HttpMessage(request);
-
-            await HttpMessageSigner.SignAsync(message, Config).ConfigureAwait(false);
-
+            await request.SignAsync(Config).ConfigureAwait(false);
             return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-        }
-
-        private class HttpMessage : IHttpMessage {
-            public HttpMessage(HttpRequestMessage request) {
-                Request = request;
-            }
-
-            public HttpContent? Content => Request.Content;
-
-            public HttpMethod Method => Request.Method;
-
-            public Uri RequestUri => Request.RequestUri!;
-
-            private HttpRequestMessage Request { get; }
-
-            public void SetHeader(string name, string value) =>
-                Request.Headers.TryAddWithoutValidation(name, value);
-
-            public bool TryGetHeaderValues(string name, [NotNullWhen(true)] out IEnumerable<string>? values) =>
-                Request.Headers.TryGetValues(name, out values);
-
-            public void SetProperty(string name, string value) =>
-#if NETSTANDARD2_0
-                Request.Properties[name] = value;
-#else
-                Request.Options.Set(new HttpRequestOptionsKey<string>(name), value);
-#endif
         }
     }
 }
