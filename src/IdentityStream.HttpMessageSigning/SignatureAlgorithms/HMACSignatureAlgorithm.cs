99922ce22af5a77171121e798d5b028cb87f60ef
Ragnar Stølsmark <ragnar@identitystream.com>
2021-08-17 16:50:16 +0200

    Added HMAC signature algorithm

---
+++ src/IdentityStream.HttpMessageSigning/SignatureAlgorithms/HMACSignatureAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace IdentityStream.HttpMessageSigning
+{
+	internal class HMACSignatureAlgorithm : ISignatureAlgorithm {
+		public HMACSignatureAlgorithm(byte[] key, HashAlgorithmName hashAlgorithm) {
+			HashAlgorithm = hashAlgorithm;
+			_key = key;
+		}
+		public string Name => "HMAC";
+
+		private byte[] _key;
+
+		public HashAlgorithmName HashAlgorithm { get;}
+
+		public byte[] ComputeHash(byte[] bytes) {
+			using var hmac = HMAC.Create(Name + HashAlgorithm.ToString());
+			if (hmac is null) {
+				throw new InvalidOperationException($"Invalid hash algorithm: {HashAlgorithm.Name}");
+			}
+			hmac.Key = _key;
+			return hmac.ComputeHash(bytes);
+		}
+	}
+}
